cb3858efa78ee81c5e477a4836d869a54bf5dbfb
n3mochaken <ns.mixa@gmail.com>
2024-03-04 13:40:51 +0300

    1

--- src/main/java/ru/hogwards/school/model/Faculty.java
+++ src/main/java/ru/hogwards/school/model/Faculty.java
@@ -1,5 +1,7 @@
 package ru.hogwards.school.model;
 
+import java.util.Objects;
+
 public class Faculty {
     private long id;
     private String name;
@@ -9,6 +11,28 @@ public class Faculty {
         this.id = id;
         this.name = name;
         this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Faculty{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Faculty faculty = (Faculty) o;
+        return id == faculty.id && Objects.equals(name, faculty.name) && Objects.equals(color, faculty.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, color);
     }
 
     public long getId() {
--- src/main/java/ru/hogwards/school/model/Student.java
+++ src/main/java/ru/hogwards/school/model/Student.java
@@ -1,10 +1,34 @@
 package ru.hogwards.school.model;
 
+import java.util.Objects;
+
 public class Student {
     public Student(Long id, String name, int age) {
         this.id = id;
         this.name = name;
         this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && Objects.equals(id, student.id) && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
     }
 
     public Long getId() {
